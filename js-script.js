36721f869c43aabf99081898d7f44b30251676de
lunarsceals <lunarsceals@gmail.com>
2023-12-28 11:00:25 -0500

    [+] Add: add number grid

--- js-script.js
+++ js-script.js
@@ -2,23 +2,22 @@
 
 
 
-const grid = document.querySelector(".container-grid");
-
+const grid = document.querySelector(".numpad");
 
 function createGrid(){
 
-    const width = 4;
-    const height = 5;
-    grid.style.gridTemplateColumns = `repeat(${width}, 1fr)`;
-    grid.style.gridTemplateRows = `repeat(${height}, 1fr)`;
-
-    for(let i=0; i<(width*height); i++){
+    const size = 3;
+    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    
+    for(let i=(size**2); i>0; i--){
+    //for (let i=1; i<=(size**2); i++){
         const tile = document.createElement("button");
         tile.classList.add("grid-tile");
         tile.textContent = i;
-        //grid.appendChild(tile);
+        grid.appendChild(tile);
     }
-
+    
 
 
 
