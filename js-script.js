0b88c549c5528281f75164de5eacd81128bc38a7
lunarsceals <lunarsceals@gmail.com>
2023-12-28 13:57:09 -0500

    [^] Update: refactor calculator buttons

--- js-script.js
+++ js-script.js
@@ -1,29 +1,14 @@
+ 
 
+const numpad_grid = document.querySelector(".numpad");
 
-
-
-const grid = document.querySelector(".numpad");
-
-function createGrid(){
-
-    const size = 3;
-    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
-    
-    for(let i=(size**2); i>0; i--){
-    //for (let i=1; i<=(size**2); i++){
+function create_grid(){
+    for (i=9; i>0; i--){
         const tile = document.createElement("button");
-        tile.classList.add("grid-tile");
         tile.textContent = i;
-        grid.appendChild(tile);
+        numpad_grid.appendChild(tile);
     }
-    
-
-
-
 }
 
 
-
-
-window.onload = () => {createGrid()};
+window.onload = () => {create_grid()}
